8633f173d20c98ce7ab0233cb358014788c0190b
Shubham011109 <shubham.vig11@gmail.com>
2024-06-27 23:17:53 +0530

    updatE

---
+++ _tests_/index1.test.js
@@ -0,0 +1,7 @@
+const { testin1 } = require('../index1')
+
+
+test('checking multiplication', () => {
+    expect(testin1(2, 3)).toBe(6);
+
+})
---
+++ index1.js
@@ -0,0 +1,6 @@
+function testin1(a, b) {
+    return a * b;
+}
+
+
+module.exports = { testin1 }
