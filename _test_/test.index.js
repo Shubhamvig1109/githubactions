9ef40873c38d4af46895230f36aded8123bfdfdd
Shubham011109 <shubham.vig11@gmail.com>
2024-06-27 23:01:33 +0530

    updatE

---
+++ _test_/test.index.js
@@ -0,0 +1,6 @@
+// __tests__/index.test.js
+import { testingAddition } from '../index';
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(testingAddition(2, 3)).toBe(5);
+});
--- test.js
+++
@@ -1,8 +0,0 @@
-const { testingAddition } = require('./index.js')
-
-
-test('addition', () => {
-    exprect(testingAddition(2, 3).toBe(5))
-})
-
-
